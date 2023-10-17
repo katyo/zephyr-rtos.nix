f0952dbe1fbad7b877b41fddea127228fc4d81ca
Otavio Salvador <otavio@ossystems.com.br>
2023-10-17 12:07:59 -0300

    flake: fix overlays definition
    I did the overlays wrong initially and nix flake check wasn't running
    fine, now it works as expected.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- flake.nix
+++ flake.nix
@@ -7,28 +7,22 @@
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-        overlays = [ self.overlays.${system}.default ];
-      });
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlays.default ]; });
     in
     {
-      overlays = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = final: prev: {
-            mkZephyrSdk =
-              { toolchains ? "all"
-              , inputs ? [ ]
-              }:
-              import ./shell.nix {
-                inherit toolchains inputs;
-                pkgs = import ./packages.nix { inherit pkgs; };
-              };
-          };
-        });
+      overlays = {
+        default = final: prev: {
+          mkZephyrSdk =
+            { toolchains ? "all"
+            , inputs ? [ ]
+            }:
+            import ./shell.nix {
+              inherit toolchains inputs;
+              pkgs = (import ./packages.nix { pkgs = prev; });
+            };
+        };
+      };
+
       devShells = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
