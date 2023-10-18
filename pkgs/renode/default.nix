0a81db016802036bae7d3bc23c421bc05337a255
Otavio Salvador <otavio@ossystems.com.br>
2023-10-18 14:25:45 -0300

    renode: 1.13.2 -> 1.14.0
    Diff: https://github.com/renode/renode/compare/None...v1.14.0

--- pkgs/renode/default.nix
+++ pkgs/renode/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, mono, gtk-sharp-2_0, screen, autoPatchelfHook, nix-update-script }:
 stdenv.mkDerivation rec {
   pname = "renode";
-  version = "1.13.2";
+  version = "1.14.0";
 
   src = fetchurl {
     url = "https://github.com/${pname}/${pname}/releases/download/v${version}/${pname}-${version}.linux-portable.tar.gz";
-    hash = "sha256-OvOlOELZ1eR3DURCoPe+WCvVyVm6DPKNcC1V7uauCjY=";
+    hash = "sha256-1wfVHtCYc99ACz8m2XEg1R0nIDh9xP4ffV/vxeeEHxE=";
   };
 
   passthru = {
