b2f89685042bb8868dd19c1cbbc72325520855b7
Otavio Salvador <otavio@ossystems.com.br>
2023-11-02 16:34:58 -0300

    flake: add templates
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ templates/arm/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/23.05";
+
+  inputs.zephyr-rtos = {
+    url = "github:katyo/zephyr-rtos.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, zephyr-rtos, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ zephyr-rtos.overlays.default ];
+      });
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = nixpkgsFor.${system}.mkZephyrSdk {
+          toolchains = [ "arm-zephyr-eabi" ];
+        };
+      });
+    };
+}
